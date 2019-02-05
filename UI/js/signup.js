18178791800a69a16a8477604b6672452dfb60a4
Jean Mark <kabohajeanmark@gmail.com>
2019-02-05 14:24:29 +0300

    Add form validation for phone number and proper name for js file[finishes #163723105]

--- UI/js/signup.js
+++ UI/js/signup.js
@@ -1,10 +1,11 @@
 function regUser(){
-    var fname=document.getElementById('firstName').value;
-	var lname=document.getElementById('lastName').value;
-	var oname=document.getElementById('otherName').value;
-	var username=document.getElementById('username').value;
+    var fname = document.getElementById('firstName').value;
+	var lname = document.getElementById('lastName').value;
+	var oname = document.getElementById('otherName').value;
+    var username = document.getElementById('username').value;
+    var phonenumber = document.getElementById('phonenumber').value;
     var email = document.getElementById("email").value;
-    var password=document.getElementById('password').value;
+    var password = document.getElementById('password').value;
 
 
     //form input validation
@@ -22,6 +23,10 @@ function regUser(){
     }
     if(username ==''){
         alert("please fill in your username");
+        return false
+    }
+    if (phonenumber == ''){
+        alert("please fill in your phone number");
         return false
     }
     else if(email== ''){
