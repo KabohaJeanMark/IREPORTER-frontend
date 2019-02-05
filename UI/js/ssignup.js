604f9723f38051c687ce4af37d950ccb7870c230
Jean Mark <kabohajeanmark@gmail.com>
2019-02-05 14:19:49 +0300

    Add form validation for user inputs on registration

---
+++ UI/js/ssignup.js
@@ -0,0 +1,34 @@
+function regUser(){
+    var fname=document.getElementById('firstName').value;
+	var lname=document.getElementById('lastName').value;
+	var oname=document.getElementById('otherName').value;
+	var username=document.getElementById('username').value;
+    var email = document.getElementById("email").value;
+    var password=document.getElementById('password').value;
+
+
+    //form input validation
+    if(fname ==''){
+        alert("please fill in your first name");
+        return false
+    }
+    if(lname ==''){
+        alert("please fill in your last name");
+        return false
+    }
+    if(oname ==''){
+        alert("please fill in your other name");
+        return false
+    }
+    if(username ==''){
+        alert("please fill in your username");
+        return false
+    }
+    else if(email== ''){
+        alert("please fill in your enter email");
+        return false
+    }else if(password== ''){
+        alert("please enter your password");
+        return false
+    }
+}
